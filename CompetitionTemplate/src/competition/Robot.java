8e411ca21ed11e8738342da24644212a25486830
Alex Schokking <alexs@alexs.local>
2015-10-03 15:27:57 -0700

    Cleaning up paths

---
+++ CompetitionTemplate/src/competition/Robot.java
@@ -0,0 +1,28 @@
+
+package competition;
+
+import xbot.common.properties.PropertyManager;
+import xbot.common.wpi_extensions.BaseRobot;
+import xbot.common.wpi_extensions.XScheduler;
+import competition.commands.ExampleCommand;
+import competition.operatorinterface.OI;
+import competition.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends BaseRobot {
+
+	@Override
+	protected void initializeSystems() {
+		super.initializeSystems();
+    }
+}
