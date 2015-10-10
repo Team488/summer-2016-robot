9966e7da8e077b5ab3339dc0ba6907e0ba77c7c6
Alex Schokking <alexs@brandverity.com>
2015-10-10 13:41:40 -0700

    Add sample drive test

---
+++ CompetitionTemplate/tests/competition/subsystems/drive/DriveSubsystemTest.java
@@ -0,0 +1,32 @@
+package competition.subsystems.drive;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import xbot.common.injection.BaseWPITest;
+
+public class DriveSubsystemTest extends BaseWPITest {
+	@Test
+	public void testTankDrive() {
+		DriveSubsystem driveSubsystem = this.injector.getInstance(DriveSubsystem.class);
+		driveSubsystem.tankDrive(1, 1);
+		
+		assertEquals(
+				this.mockRobotIO.getPWM(driveSubsystem.leftFrontDrive.getChannel()),
+				1,
+				0.001);
+		assertEquals(
+				this.mockRobotIO.getPWM(driveSubsystem.leftRearDrive.getChannel()),
+				1,
+				0.001);
+		assertEquals(
+				this.mockRobotIO.getPWM(driveSubsystem.rightFrontDrive.getChannel()),
+				1,
+				0.001);
+		assertEquals(
+				this.mockRobotIO.getPWM(driveSubsystem.rightRearDrive.getChannel()),
+				1,
+				0.001);
+	}
+}
