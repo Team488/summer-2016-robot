95ca1fc010755bcfcf90000cf3b57dac19c3e33a
Alex Schokking <alexs@alexs.local>
2016-01-16 10:51:25 -0800

    Remove IMU validation code from the base FRCTemplate, it throws a lot of errors if no NavX is present, we'll want to come up with a different way of storing this

--- CompetitionTemplate/src/competition/subsystems/SubsystemDefaultCommandMap.java
+++ CompetitionTemplate/src/competition/subsystems/SubsystemDefaultCommandMap.java
@@ -3,26 +3,18 @@ package competition.subsystems;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
-import competition.subsystems.sensor.ValidateIMUSubsystem;
-import competition.subsystems.sensor.commands.ValidateIMUCommand;
+import competition.subsystems.drive.DriveSubsystem;
+import competition.subsystems.drive.commands.TankDriveWithJoysticksCommand;
 
 @Singleton
 public class SubsystemDefaultCommandMap {
 	// For setting the default commands on subsystems
 	
-	/*@Inject
+	@Inject
 	public void setupDriveSubsystem(
 	        DriveSubsystem driveSubsystem,
 	        TankDriveWithJoysticksCommand command)
 	{
 		driveSubsystem.setDefaultCommand(command);
-	}*/
-	
-   @Inject
-    public void setupSensorSubsystem(
-            ValidateIMUSubsystem sensorSubsystem,
-            ValidateIMUCommand command)
-    {
-       sensorSubsystem.setDefaultCommand(command);
-    }
+	}
 }
