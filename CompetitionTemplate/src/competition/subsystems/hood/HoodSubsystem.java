59d3544bd1983e2c7c1246f22e9bf023c6ce6659
SethNguyenXBOT <sensaiwu10@gmail.com>
2016-11-05 12:40:48 -0700

    Changed double "tab" to single "tab".

--- CompetitionTemplate/src/competition/subsystems/hood/HoodSubsystem.java
+++ CompetitionTemplate/src/competition/subsystems/hood/HoodSubsystem.java
@@ -56,17 +56,17 @@ public class HoodSubsystem extends BaseSubsystem{
     }
     
     public void setRotationPower(double power) {
-                hoodPosition.set(hoodMotor.getEncoderPosition());
+        hoodPosition.set(hoodMotor.getEncoderPosition());
         
-                if(hoodPosition.get() > hoodExtendPosition.get()){
-                    setRotationPower(0);
-                }
+        if(hoodPosition.get() > hoodExtendPosition.get()){
+            setRotationPower(0);
+        }
                 
-                if(hoodPosition.get() < hoodRetractPosition.get()){
-                    setRotationPower(0);
-                }
+        if(hoodPosition.get() < hoodRetractPosition.get()){
+            setRotationPower(0);
+        }        
               
-                 hoodMotor.set(power);
-             }
+        hoodMotor.set(power);
+        }         
 }
 
